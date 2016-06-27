7c0646ef6e036378a869b1a7652a8babb3d01b9e
Hider-The-First <orenvilderman@walla.co.il>
2016-06-27 23:24:33 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -2115,7 +2115,7 @@ function autoMap() {
         }
 
         //map if we don't have health/dmg or we need to clear void maps or if we are prestige mapping, and our set item has a new prestige available 
-        if ((shouldDoMaps || doVoids || needPrestige) && (game.resources.trimps.owned > game.resources.trimps.realMax() * 0.9999 || game.global.lastClearedCell > 81)) {
+        if ((shouldDoMaps || doVoids || needPrestige) && ((!game.global.mapsActive && game.resources.trimps.owned > game.resources.trimps.realMax() * 0.9999) || game.global.lastClearedCell > 81)) {
             //shouldDoMap = world here if we haven't set it to create yet, meaning we found appropriate high level map, or siphon map
 
             if (shouldDoMap == "world") {
@@ -2174,7 +2174,7 @@ function autoMap() {
                 if (shouldDoMap != "world") {
                     //if shouldFarm, don't switch until after megamining //genBTC changed.
                     if (!game.global.switchToMaps){
-                        if ((shouldDoMaps && game.global.lastClearedCell < 10 && game.resources.trimps.owned > game.resources.trimps.realMax() * 0.9999) || (shouldFarm && game.global.lastClearedCell >= 81) || (needPrestige && game.resources.trimps.owned > game.resources.trimps.realMax() * 0.995)|| doVoids || shouldDoMap!="world")
+                        if ((shouldDoMaps && game.global.lastClearedCell < 10 && (!game.global.mapsActive && game.resources.trimps.owned > game.resources.trimps.realMax() * 0.9999)) || (shouldFarm && game.global.lastClearedCell >= 81) || (needPrestige && (!game.global.mapsActive && game.resources.trimps.owned > game.resources.trimps.realMax() * 0.995)) || doVoids || shouldDoMap!="world")
                             mapsClicked();
                     }
                     ////Get Impatient/Abandon if: need prestige / _NEED_ to do void maps / on lead in odd world. AND a new army is ready, OR _need_ to void map OR lead farming and we're almost done with the zone )
