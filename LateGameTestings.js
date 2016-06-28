cc52212e4a8aa0532133afdc5ca082778fe7a034
Hider-The-First <orenvilderman@walla.co.il>
2016-06-28 03:57:28 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -2190,10 +2190,9 @@ function autoMap() {
                             mapsClicked();
                     }
                     ////Get Impatient/Abandon if: need prestige / _NEED_ to do void maps / on lead in odd world. AND a new army is ready, OR _need_ to void map OR lead farming and we're almost done with the zone )
-                    if(
-                        game.global.switchToMaps 
+                    if(game.global.switchToMaps 
                         && (game.global.world !=200 || (game.global.world ==200 && game.global.soldierHealth ==0))
-                        (needPrestige || doVoids || (game.global.challengeActive == 'Lead' && game.global.world % 2 == 1)) 
+                        &&(needPrestige || doVoids || (game.global.challengeActive == 'Lead' && game.global.world % 2 == 1)) 
                         && 
                             (
                             (game.resources.trimps.realMax() <= game.resources.trimps.owned + 1)
