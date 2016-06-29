87b26f0f9ab67b4095f52cd7472d5da860b59b3d
Hider-The-First <orenvilderman@walla.co.il>
2016-06-29 23:40:08 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -1098,8 +1098,8 @@ function buyStorage() {
            document.getElementById('Prestige').selectedIndex = 13;
            autoTrimpSettings.Prestige.selected = "GambesOP";
        } else if (game.global.world > 200) {
-           document.getElementById('Prestige').selectedIndex = 2;
-           autoTrimpSettings.Prestige.selected = "Dagadder";
+           document.getElementById('Prestige').selectedIndex = 0;
+           autoTrimpSettings.Prestige.selected = "Off";
        }
        //}
        //if (game.global.world == 200 || game.global.world == 201) {
