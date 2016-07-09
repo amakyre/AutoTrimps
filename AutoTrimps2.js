a2b94e22338550cb1ad8136554997afbcaea56da
amakyre <strid.jimmy@gmail.com>
2016-07-09 20:17:44 +0200

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -1777,8 +1777,7 @@ function autoMap() {
                    } else if (game.global.world == 200 && game.global.lastClearedCell > 70) {
                       document.getElementById('Prestige').selectedIndex = 6;
                        autoTrimpSettings.Prestige.selected = "Polierarm";
-                   }
-                   if (game.global.world == 201) {
+                   } else if (game.global.world == 200 && game.global.lastClearedCell > 90) {
                        document.getElementById('Prestige').selectedIndex = 4;
                        autoTrimpSettings.Prestige.selected = "Megamace";
                    }   
