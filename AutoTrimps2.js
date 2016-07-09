3b785fedb201041f2a3ba76975f87874dc3e72af
amakyre <strid.jimmy@gmail.com>
2016-07-09 21:04:01 +0200

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -1775,8 +1775,8 @@ function autoMap() {
                          document.getElementById('Prestige').selectedIndex = 5;
                            autoTrimpSettings.Prestige.selected = "Hellishmet";
                    } else if (game.global.world == 200 && game.global.lastClearedCell > 70) {
-                      document.getElementById('Prestige').selectedIndex = 6;
-                       autoTrimpSettings.Prestige.selected = "Polierarm";
+                      document.getElementById('Prestige').selectedIndex = 7;
+                       autoTrimpSettings.Prestige.selected = "Pantastic";
                    } else if (game.global.world == 200 && game.global.lastClearedCell > 90) {
                        document.getElementById('Prestige').selectedIndex = 4;
                        autoTrimpSettings.Prestige.selected = "Megamace";
