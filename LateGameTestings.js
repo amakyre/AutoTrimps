9a4a8918f0377a6685d8f1950c8b56dbce030ad5
Hider-The-First <orenvilderman@walla.co.il>
2016-07-10 05:17:45 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -1857,9 +1857,9 @@ function autoMap() {
 //        (game.global.world >= 22// && game.global.world <= 230 && game.global.mapBonus <= 2) ||
 //        (game.global.world == 220 && game.global.mapBonus <= 3) ||
 //        (game.global.world >= 210 && game.global.lastClearedCell > 93 && ((new Date().getTime() - game.global.mapStarted > 8000 && game.global.mapsActive) || game.global.mapBonus <= 9 )) ||
-        ((game.global.world == 230 || game.global.world == 200) && game.global.lastClearedCell > 70 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 3) ||
-        ((game.global.world >= 223 && game.global.world < 230 ) && game.global.mapBonus < 2) ||
-        ((game.global.world == 217 || game.global.world == 222 || game.global.world == 219) && game.global.mapBonus < 1 && !game.global.mapsActive)) {  //didnt work (game.global.mapBonus < game.global.world-(game.upgrades.Coordinated.level+1)
+        ((game.global.world == 235 || game.global.world == 200) && game.global.lastClearedCell > 70 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 3) ||
+        ((game.global.world >= 228 && game.global.world < 235 ) && game.global.mapBonus < 2) ||
+        ((game.global.world == 222 || game.global.world == 227 || game.global.world == 224) && game.global.mapBonus < 1 && !game.global.mapsActive)) {  //didnt work (game.global.mapBonus < game.global.world-(game.upgrades.Coordinated.level+1)
         //(((((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 80) || game.global.world > 244) && game.global.lastClearedCell > 93 && game.global.world > 225)) {
             shouldDoMaps = true;
             shouldDoNullMaps = true;
